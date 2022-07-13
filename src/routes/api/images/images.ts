38df6bc127d39ca69686d76c06203eb0952a7ace
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2022-07-13 09:01:24 +0000

    Completed functionality

--- src/routes/api/images/images.ts
+++ src/routes/api/images/images.ts
@@ -4,11 +4,12 @@ import resizeImage from '../../../utilities/imageResize';
 const images = express.Router();
 
 images.get('/', (req, res) => {
-  const importedFileName = `${req.query.filename}.jpg`;
-  const exportedFileName = `${req.query.filename}_thumb.jpg`;
+  const importedFileName = `images/${req.query.filename}.jpg`;
+  const exportedFileName = `converted-images/${req.query.filename}_thumb.jpg`;
   const height = Number(req.query.height);
   const width = Number(req.query.width);
   resizeImage(importedFileName, exportedFileName, height, width);
+  res.sendStatus(200);
 });
 
 export default images;
